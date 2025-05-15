c113ebdb1ac116a5ba0a18446e8bf4fea88acb83
Mace Akkari <mace.akkari@relyfe.com>
2025-05-15 16:25:19 +0100

    chore: update tailwind config and globals

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xl: "1.5rem",
+        "2xl": "1.75rem",
+      },
+      colors: {
+        background: "var(--brr-bg-color)",
+        text: "var(--brr-text-color)",
+        "text-muted": "var(--brr-text-muted)",
+        muted: "var(--brr-muted)",
+        border: "var(--brr-border-color)",
+        primary: "var(--brr-primary)",
+      },
+    },
   },
   plugins: [],
 };
